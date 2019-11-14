8ee8ee5cf83d0d8675c647f2d429e0c4976869eb
Matt Jaeger <matt.jaeger@outlook.com>
2019-11-13 17:03:51 -0800

    Implement register

--- Incite.Discord/Commands/MemberCommands.cs
+++ Incite.Discord/Commands/MemberCommands.cs
@@ -21,10 +21,10 @@ namespace Incite.Discord.Commands
         {
             await context.Message.DeleteAsync();
 
-            var channel = (await context.Member?.CreateDmChannelAsync()) ?? context.Channel;
+            var channel = await context.Member.CreateDmChannelAsync();
             await channel.TriggerTypingAsync();
 
-            using var dbContext = context.Client.GetExtension<DatabaseExtension>().GetDbContext();
+            using var dbContext = new InciteDbContext();
 
             var members = dbContext.Members
                 .Where(x => x.Guild.DiscordGuildId == context.Guild.Id);
@@ -45,30 +45,37 @@ namespace Incite.Discord.Commands
 
         [Command("register")]
         [Description("Lists the official guild raid days and times")]
-        public async Task Register(CommandContext context)
+        public async Task Register(CommandContext context, string primaryCharacterName)
         {
             await context.Message.DeleteAsync();
 
-            var channel = (await context.Member?.CreateDmChannelAsync()) ?? context.Channel;
+            var channel = await context.Member.CreateDmChannelAsync();
             await channel.TriggerTypingAsync();
 
-            using var dbContext = context.Client.GetExtension<DatabaseExtension>().GetDbContext();
+            using var dbContext = new InciteDbContext();
 
-            var members = dbContext.Members
-                .Where(x => x.Guild.DiscordGuildId == context.Guild.Id);
+            var guild = dbContext.Guilds
+                .First(x => x.DiscordGuildId == context.Guild.Id);
 
-            StringBuilder memberList = new StringBuilder("Character Name : Discord Name");
+            var member = dbContext.Members
+                .FirstOrDefault(x => x.Guild.DiscordGuildId == context.Guild.Id && x.DiscordUserId == context.Member.Id);
 
-            foreach (var member in members)
+            if (member == null)
             {
-                string discordName = context.Guild.Members.ContainsKey(member.DiscordUserId) ?
-                    context.Guild.Members[member.DiscordUserId].Nickname :
-                    "(Unknown)";
-
-                memberList.AppendLine($"{member.PrimaryCharacterName} : {discordName}");
+                dbContext.Add(new Member()
+                {
+                    GuildId = guild.Id,
+                    PrimaryCharacterName = primaryCharacterName
+                });
+            }
+            else
+            {
+                member.PrimaryCharacterName = primaryCharacterName;
+                dbContext.Update(member);
             }
 
-            await channel.SendMessageAsync(memberList.ToString());
+            await dbContext.SaveChangesAsync();
+            await channel.SendMessageAsync("Success!");
         }
     }
 }
