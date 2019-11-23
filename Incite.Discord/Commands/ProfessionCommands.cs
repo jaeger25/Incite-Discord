6b55e9a048a408bda8716211a0ce33bff872c585
Matt Jaeger <matt.jaeger@outlook.com>
2019-11-22 19:21:36 -0800

    Fix help string. Add Profession skeleton.

---
+++ Incite.Discord/Commands/ProfessionCommands.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using Incite.Discord.Attributes;
+using Incite.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Incite.Discord.Commands
+{
+    [Group("profession")]
+    [RequireGuildConfigured]
+    [Description("Commands for managing guild members")]
+    public class ProfessionCommands : BaseCommandModule
+    {
+        readonly InciteDbContext m_dbContext;
+
+        public ProfessionCommands(InciteDbContext dbContext)
+        {
+            m_dbContext = dbContext;
+        }
+    }
+}
