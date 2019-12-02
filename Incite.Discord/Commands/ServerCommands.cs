e1cac429a1e234017a97277787233da79c6a2aff
Matt Jaeger <matt.jaeger@outlook.com>
2019-12-01 20:59:12 -0800

    Fix bug in !server list profession

--- Incite.Discord/Commands/ServerCommands.cs
+++ Incite.Discord/Commands/ServerCommands.cs
@@ -49,9 +49,7 @@ namespace Incite.Discord.Commands
         {
             var characters = Guild.WowServer.WowCharacters
                 .Where(x => x.WowCharacterProfessions
-                    .Any(x => x.WowProfessionId == profession.Id))
-                .OrderBy(x => x.WowCharacterProfessions
-                    .Select(x => x.WowCharacterRecipes.Count));
+                    .Any(x => x.WowProfessionId == profession.Id));
 
             StringBuilder message = new StringBuilder($"__**{profession}**__\n");
             foreach (var character in characters)
