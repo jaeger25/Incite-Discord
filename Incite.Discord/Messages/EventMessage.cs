34715a22a073502d76b447d1817499622cc0016e
Matt Jaeger <matt.jaeger@outlook.com>
2019-12-06 22:11:09 -0800

    Timezone in EventMessage

--- Incite.Discord/Messages/EventMessage.cs
+++ Incite.Discord/Messages/EventMessage.cs
@@ -104,7 +104,7 @@ namespace Incite.Discord.Messages
             // Player count and date
             AddEventField(embed, $"{m_emojis.Events.Icon_Count}", $"{guildEvent.EventMembers.Count(x => x.EmojiDiscordName != m_emojis.Events.Icon_Late.Name && x.EmojiDiscordName != m_emojis.Events.Icon_Maybe.Name && x.EmojiDiscordName != m_emojis.Events.Icon_Absent.Name )}");
             AddEventField(embed, $"{m_emojis.Events.Icon_Date}", $"{guildEvent.DateTime.ToString("MM-dd")}");
-            AddEventField(embed, $"{m_emojis.Events.Icon_Time}", $"{guildEvent.DateTime.ToString("h:mm tt zzz")}");
+            AddEventField(embed, $"{m_emojis.Events.Icon_Time}", $"{guildEvent.DateTime.ToOffset(new TimeSpan(-5, 0, 0)).ToString("h:mm tt zzz")}");
 
             // Melee, Ranged, Healer counts
             AddEventField(embed, $"{m_emojis.Events.Role_Tank}", $"{guildEvent.EventMembers.Count(x => m_emojis.TankEmojis().Select(x => x.Name).Contains(x.EmojiDiscordName))}");
